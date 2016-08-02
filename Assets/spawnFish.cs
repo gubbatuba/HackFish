d59c924ec5a25630f16b95ad0250ca214d9525f9
zhengster <zandrew0@gmail.com>
2016-08-02 14:39:56 -0400

    master version with fish spawn

---
+++ Assets/spawnFish.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spawnFish : MonoBehaviour {
+
+    public GameObject fish;
+    public float spawnTime = 3f;
+    public Transform[] spawnPoints;
+    public float maxFish = 20f;
+
+    private int numFish;
+
+	// Use this for initialization
+	void Start ()
+    {
+        numFish = 0;
+        InvokeRepeating("Spawn", spawnTime, spawnTime);
+	}
+
+    void Spawn()
+    {
+        if (numFish <= maxFish)
+        {
+            numFish = numFish + 1;
+            int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+            Instantiate(fish, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+        }
+    }
+}
