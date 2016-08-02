fbf8a64d750177f57be695f8f643e7671234553e
zhengster <zandrew0@gmail.com>
2016-08-02 10:26:18 -0400

    fish and bobble

---
+++ Assets/fishMove.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fishMove: MonoBehaviour
+{
+    Transform bobble;
+    NavMeshAgent nav;
+
+
+    void Awake()
+    {
+        bobble = GameObject.FindGameObjectWithTag("Bobble").transform;
+        nav = GetComponent<NavMeshAgent>();
+    }
+
+
+    void Update()
+    {
+        nav.SetDestination(bobble.position);
+    }
+}
