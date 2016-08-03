97ebc8c05b79d80c7cd79d736d723fbecb973826
dadoist <dramadani@shelltechworks.com>
2016-08-03 16:19:23 -0400

    FISH ARE UNDERWATER

--- Assets/Scripts/fishMove.cs
+++ Assets/Scripts/fishMove.cs
@@ -16,6 +16,6 @@ public class fishMove: MonoBehaviour
 
     void Update()
     {
-        nav.SetDestination(bobble.position);
+        nav.SetDestination(new Vector3(bobble.position.x, bobble.position.y-(float)10, bobble.position.z));
     }
 }
