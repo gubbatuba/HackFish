cd5ed0d6d5e7693f31994843ab26184ef362ea44
dadoist <dramadani@shelltechworks.com>
2016-08-03 20:46:55 -0400

    adding assets and fixing fishing

--- Assets/Scripts/stabFish.cs
+++ Assets/Scripts/stabFish.cs
@@ -6,9 +6,13 @@ public class stabFish: MonoBehaviour {
 
 
     Animator anim;
+    NavMeshAgent navm;
+    Rigidbody fishBody;
     void Awake()
     {
         anim = GetComponent<Animator>();
+        navm = GetComponent<NavMeshAgent>();
+        fishBody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -24,6 +28,8 @@ public class stabFish: MonoBehaviour {
             spawnFish.numFish = spawnFish.numFish - 1;
             Debug.Log("Fish count = " + spawnFish.numFish);
             spawnFish.score += 1;
+            Destroy(navm);
+            Destroy(fishBody);
             gameObject.transform.SetParent(col.gameObject.transform);
             anim.SetTrigger("FishDead");
             Destroy(gameObject, 4.5f);
